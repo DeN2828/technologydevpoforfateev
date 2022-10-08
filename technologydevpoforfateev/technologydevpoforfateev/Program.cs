1e46d158acc49420c690865b7ff9975a91e5e4d7
DeN2828 <den-tu@ua.fm>
2022-10-08 13:26:16 +0300

    count gram in liters

---
+++ technologydevpoforfateev/technologydevpoforfateev/Program.cs
@@ -0,0 +1,6 @@
+﻿//Console.WriteLine("Enter meter: ");
+//Console.WriteLine($"{int.Parse(Console.ReadLine()) * 100} centimeter");
+//Console.WriteLine("Enter meter: ");
+//Console.WriteLine($"{float.Parse(Console.ReadLine()) / 1000} kilometers");
+Console.WriteLine("Enter gramm: ");
+Console.WriteLine($"{float.Parse(Console.ReadLine()) / 1000} liters");
