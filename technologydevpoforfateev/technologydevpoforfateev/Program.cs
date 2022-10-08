f7c6be2cb78296dd22058d28630a5287ebbdccb1
DeN2828 <den-tu@ua.fm>
2022-10-08 13:31:10 +0300

    Версия 1.0.0.2

--- technologydevpoforfateev/technologydevpoforfateev/Program.cs
+++ technologydevpoforfateev/technologydevpoforfateev/Program.cs
@@ -1,4 +1,15 @@
+<<<<<<< Updated upstream
 ﻿Console.WriteLine("Enter meter: ");
 Console.WriteLine($"{int.Parse(Console.ReadLine()) * 100} centimeter");
 Console.WriteLine("Enter meter: ");
 Console.WriteLine($"{float.Parse(Console.ReadLine()) / 1000} kilometers");
+=======
+﻿//Console.WriteLine("Enter meter: ");
+//Console.WriteLine($"{int.Parse(Console.ReadLine()) * 100} centimeter");
+//Console.WriteLine("Enter meter: ");
+//Console.WriteLine($"{float.Parse(Console.ReadLine()) / 1000} kilometers");
+Console.WriteLine("Enter gramm: ");
+Console.WriteLine($"{float.Parse(Console.ReadLine()) / 1000} liters");
+Console.WriteLine("Enter liters: ");
+Console.WriteLine($"{float.Parse(Console.ReadLine()) / 3,785} gallons");
+>>>>>>> Stashed changes
