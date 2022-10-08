75832d8a71915cdd71176d97259704716ca73bb6
DeN2828 <den-tu@ua.fm>
2022-10-08 13:22:22 +0300

    Версия 1.0.0.1

---
+++ technologydevpoforfateev/technologydevpoforfateev/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Enter meter: ");
+Console.WriteLine($"{int.Parse(Console.ReadLine()) * 100} centimeter");
+Console.WriteLine("Enter meter: ");
+Console.WriteLine($"{float.Parse(Console.ReadLine()) / 1000} kilometers");
