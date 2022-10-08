c6e998bf522ca624d3515fa84bf9be61e9460d14
DeN2828 <den-tu@ua.fm>
2022-10-08 13:37:50 +0300

    Версия 1.0.0.3

--- technologydevpoforfateev/technologydevpoforfateev/Program.cs
+++ technologydevpoforfateev/technologydevpoforfateev/Program.cs
@@ -8,8 +8,18 @@ Console.WriteLine($"{float.Parse(Console.ReadLine()) / 1000} kilometers");
 //Console.WriteLine($"{int.Parse(Console.ReadLine()) * 100} centimeter");
 //Console.WriteLine("Enter meter: ");
 //Console.WriteLine($"{float.Parse(Console.ReadLine()) / 1000} kilometers");
+<<<<<<< Updated upstream
 Console.WriteLine("Enter gramm: ");
 Console.WriteLine($"{float.Parse(Console.ReadLine()) / 1000} liters");
 Console.WriteLine("Enter liters: ");
 Console.WriteLine($"{float.Parse(Console.ReadLine()) / 3,785} gallons");
+>>>>>>> Stashed changes
+=======
+//Console.WriteLine("Enter gramm: ");
+//Console.WriteLine($"{float.Parse(Console.ReadLine()) / 1000} liters");
+//Console.WriteLine("Enter liters: ");
+//Console.WriteLine($"{float.Parse(Console.ReadLine()) / 3,785} gallons");
+// Версия 1.0.0.3
+Console.WriteLine("Enter celsia: ");
+Console.WriteLine($"{float.Parse(Console.ReadLine()) * (9/5) + 32} Farengeita");
 >>>>>>> Stashed changes
