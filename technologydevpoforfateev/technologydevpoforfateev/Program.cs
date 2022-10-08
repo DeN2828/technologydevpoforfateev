8dd491d36f452ad7bff63c7185ae5ea329a14bcf
DeN2828 <den-tu@ua.fm>
2022-10-08 13:30:22 +0300

    count liters in gallons

---
+++ technologydevpoforfateev/technologydevpoforfateev/Program.cs
@@ -0,0 +1,8 @@
+﻿//Console.WriteLine("Enter meter: ");
+//Console.WriteLine($"{int.Parse(Console.ReadLine()) * 100} centimeter");
+//Console.WriteLine("Enter meter: ");
+//Console.WriteLine($"{float.Parse(Console.ReadLine()) / 1000} kilometers");
+//Console.WriteLine("Enter gramm: ");
+//Console.WriteLine($"{float.Parse(Console.ReadLine()) / 1000} liters");
+Console.WriteLine("Enter liters: ");
+Console.WriteLine($"{float.Parse(Console.ReadLine()) / 3,785} gallons");
