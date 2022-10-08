23efa39c29afee1240f2f6086c0ab1de523339c9
DeN2828 <den-tu@ua.fm>
2022-10-08 13:35:16 +0300

    count celsia in farengeit

---
+++ technologydevpoforfateev/technologydevpoforfateev/Program.cs
@@ -0,0 +1,10 @@
+//Console.WriteLine("Enter meter: ");
+//Console.WriteLine($"{int.Parse(Console.ReadLine()) * 100} centimeter");
+//Console.WriteLine("Enter meter: ");
+//Console.WriteLine($"{float.Parse(Console.ReadLine()) / 1000} kilometers");
+//Console.WriteLine("Enter gramm: ");
+//Console.WriteLine($"{float.Parse(Console.ReadLine()) / 1000} liters");
+//Console.WriteLine("Enter liters: ");
+//Console.WriteLine($"{float.Parse(Console.ReadLine()) / 3,785} gallons");
+Console.WriteLine("Enter celsia: ");
+Console.WriteLine($"{float.Parse(Console.ReadLine()) * (9/5) + 32} Farengeita");
