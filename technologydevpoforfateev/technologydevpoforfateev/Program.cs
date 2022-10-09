d08dc58f1a41c9b09cd61bb21c75c3b1214ff805
DeN2828 <den-tu@ua.fm>
2022-10-09 17:10:14 +0300

    Update Program.cs

--- technologydevpoforfateev/technologydevpoforfateev/Program.cs
+++ technologydevpoforfateev/technologydevpoforfateev/Program.cs
@@ -1,25 +1,13 @@
-<<<<<<< Updated upstream
-ï»¿Console.WriteLine("Enter meter: ");
+// Âåğñèÿ 1.0.0.1
+Console.WriteLine("Enter meter: ");
 Console.WriteLine($"{int.Parse(Console.ReadLine()) * 100} centimeter");
 Console.WriteLine("Enter meter: ");
 Console.WriteLine($"{float.Parse(Console.ReadLine()) / 1000} kilometers");
-=======
-ï»¿//Console.WriteLine("Enter meter: ");
-//Console.WriteLine($"{int.Parse(Console.ReadLine()) * 100} centimeter");
-//Console.WriteLine("Enter meter: ");
-//Console.WriteLine($"{float.Parse(Console.ReadLine()) / 1000} kilometers");
-<<<<<<< Updated upstream
+// Âåğñèÿ 1.0.0.2
 Console.WriteLine("Enter gramm: ");
 Console.WriteLine($"{float.Parse(Console.ReadLine()) / 1000} liters");
 Console.WriteLine("Enter liters: ");
 Console.WriteLine($"{float.Parse(Console.ReadLine()) / 3,785} gallons");
->>>>>>> Stashed changes
-=======
-//Console.WriteLine("Enter gramm: ");
-//Console.WriteLine($"{float.Parse(Console.ReadLine()) / 1000} liters");
-//Console.WriteLine("Enter liters: ");
-//Console.WriteLine($"{float.Parse(Console.ReadLine()) / 3,785} gallons");
 // Âåğñèÿ 1.0.0.3
 Console.WriteLine("Enter celsia: ");
 Console.WriteLine($"{float.Parse(Console.ReadLine()) * (9/5) + 32} Farengeita");
->>>>>>> Stashed changes
